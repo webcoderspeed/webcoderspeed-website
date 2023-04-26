2115a3e7fe7a34b40a8e7caa70db2b5a3e921b32
webcoderspeed <webcoderspeed@gmail.com>
2023-04-26 21:06:14 +0530

    webpack plugin for purging

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const TerserPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -9,6 +12,33 @@ const nextConfig = {
     // Set your caching headers for client-side rendered pages
     cacheControl: 'public, max-age=60, stale-while-revalidate=86400',
   },
+  webpack: (config, { isServer }) => {
+    // Enable TerserPlugin for JS minification
+    config.optimization.minimizer.push(
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true, // Remove console.* statements
+          },
+        },
+      })
+    );
+
+    // Enable CompressionPlugin for gzip compression
+    if (!isServer) {
+      config.plugins.push(
+        new CompressionPlugin({
+          filename: '[path][base].gz',
+          algorithm: 'gzip',
+          test: /\.(js|css)$/, // Compress JS and CSS files
+          threshold: 10240, // Only compress files larger than 10KB
+          minRatio: 0.8, // Only compress files with compression ratio > 0.8
+        })
+      );
+    }
+
+    return config;
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
