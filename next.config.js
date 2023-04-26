f9a58eded6d6faa1968db6cbdcd1b6ca18b6829e
webcoderspeed <webcoderspeed@gmail.com>
2023-04-26 21:01:36 +0530

    index meta

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  serverRuntimeConfig: {
+    // Set your caching headers for server-side rendered pages
+    cacheControl: 's-maxage=60, stale-while-revalidate=86400',
+  },
+  publicRuntimeConfig: {
+    // Set your caching headers for client-side rendered pages
+    cacheControl: 'public, max-age=60, stale-while-revalidate=86400',
+  },
+};
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,40 @@
 import Image from 'next/image';
 import { Nunito } from 'next/font/google';
 import Header from '@/components/Header';
+import { NextSeo } from 'next-seo';
 
 const nunito = Nunito({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <main className={`${nunito.className}`}>
+      <NextSeo
+        title='WebCoderSpeed - Latest Tech News, Code, and AI Articles'
+        description='Stay up-to-date with the latest technology news, coding tutorials, and articles on artificial intelligence (AI) at WebCoderSpeed.'
+        canonical='https://webcoderspeed.com/'
+        openGraph={{
+          title: 'WebCoderSpeed - Latest Tech News, Code, and AI Articles',
+          description:
+            'Stay up-to-date with the latest technology news, coding tutorials, and articles on artificial intelligence (AI) at WebCoderSpeed.',
+          url: '/public/logo.png',
+          images: [
+            {
+              url: '/public/logo.png',
+              alt: 'WebCoderSpeed - Latest Tech News, Code, and AI Articles',
+            },
+          ],
+          site_name: 'WebCoderSpeed',
+        }}
+        robotsProps={{
+          nosnippet: true,
+          notranslate: true,
+          noimageindex: true,
+          noarchive: true,
+          maxSnippet: -1,
+          maxImagePreview: 'none',
+          maxVideoPreview: -1,
+        }}
+      />
       <Header />
     </main>
   );
